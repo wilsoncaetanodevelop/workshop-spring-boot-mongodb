19a417c4710ddfc2c4c482ad226fe7a7a262d2ca
wilsoncaetanodevelop <wilson-caaetano@hotmail.com>
2020-07-20 14:38:55 -0300

    Inserção de usuário com POST

--- src/main/java/com/wilson/workshopmongo/services/UserService.java
+++ src/main/java/com/wilson/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wilson.workshopmongo.domain.User;
+import com.wilson.workshopmongo.dto.UserDTO;
 import com.wilson.workshopmongo.repository.UserRepository;
 import com.wilson.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objdto) {
+		return new User(objdto.getId(),objdto.getName(),objdto.getEmail());
 	}
 }
