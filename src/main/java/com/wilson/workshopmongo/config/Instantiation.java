c4383587cfb54fa7829fe140fd7335e40a226bd2
wilsoncaetanodevelop <wilson-caaetano@hotmail.com>
2020-07-21 11:12:38 -0300

    Referenciando os posts do usuário

--- src/main/java/com/wilson/workshopmongo/config/Instantiation.java
+++ src/main/java/com/wilson/workshopmongo/config/Instantiation.java
@@ -18,7 +18,7 @@ import com.wilson.workshopmongo.repository.UserRepository;
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository UserRepository;
+	private UserRepository userRepository;
 
 	@Autowired
 	private PostRepository postRepository;
@@ -29,19 +29,22 @@ public class Instantiation implements CommandLineRunner {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-		UserRepository.deleteAll();
+		userRepository.deleteAll();
 		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		UserRepository.saveAll(Arrays.asList(maria, alex, bob));
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "Vou viajara pra são paulo. Abroços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia ", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 	}
 
 }
